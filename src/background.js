8af4c675b8adfa0e2043db593e200f60138027f5
Avanti Aswal <avanti2209@gmail.com>
2020-07-03 11:17:17 +0530

    background.js added for chrome extension

---
+++ src/background.js
@@ -0,0 +1,8 @@
+/**
+ * 1. No UI
+ * 2. Only one possible in one extension
+ * 3. controls state of you extension
+ * 4. https://medium.com/@gilfink/adding-web-interception-abilities-to-your-chrome-extension-fb42366df425
+ */
+
+console.log('background.js is running NOT in use')
